eeaf961c77c861abbf05753f68060d374b78fc63
WasayRabbani <wasayrabbani69@gmail.com>
2026-09-26 11:17:51 +0500

    Edited fetch_data

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,29 +1,41 @@
 from pathlib import Path
 
 # Main purpose is to take a text file, convert it into langchain document object
-from langchain_community.document_loaders import TextLoader
+from langchain_core.documents import Document
 
 
-# Makes any link, system link
+class fetch_data:
+    def __init__(self, path):
 
-policy_folder = Path(r"D:\Coding Stuff\SupportOps AI\data\policies")
+        self.path = Path(path)
+        # Makes any link, system link
 
-documents = []
+    def fetch_docs(self):
+
+        documents = []
+
+        # Here we fetch all the files in that link that are having .md at the back.
+
+        # every md file it finds, temporarily calls that file
+        # file is Path object (Path that we fetched earlier), not filename
+        for file in self.path.glob("*.md"):
+
+            # file -> Path("D:/Coding Stuff/.../faq.md")
+            # str(file) gives  "D:/Coding Stuff/.../faq.md" like a normal string
+
+            text = file.read_text(encoding="utf-8")
+
+            document = Document(
+                page_content=text,
+                metadata={"source": str(file)}
+            )
+
+            documents.append(document)
+
+        # for document in documents:
+        #         print(document.page_content)
+        return (documents[0].page_content)
 
 
-# Here we fetch all the files in that link that are having .md at the back.
-
-# every md file it finds, temporarily calls that file
-# file is Path object (Path that we fetched earlier), not filename
-for file in policy_folder.glob("*.md"):
-
-    # file -> Path("D:/Coding Stuff/.../faq.md")
-    # str(file) gives  "D:/Coding Stuff/.../faq.md" like a normal string
-    # Then we give it to TextLoader and it loads the documents  but when we use .load , it shows to user
-
-    # Now we need to add extracted documents into list
-    documents.extend(TextLoader(str(file)).load())
-
-# for document in documents:
-#         print(document.page_content)
-print(documents[0].page_content)
+obj = fetch_data(r"D:\Coding Stuff\SupportOps AI\data\policies")
+print(obj.fetch_docs())
